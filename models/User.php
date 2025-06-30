8a4621546af1b27937337c09440626aedb456793
rodrigorochalima <rodrigorochalima@gmail.com>
2025-06-29 23:44:26 -0300

    Delete models directory

--- models/User.php
+++
@@ -1,1 +0,0 @@
-<?php class User extends \yii\web\IdentityInterface { public static function findIdentity($id) { return null; } public static function findIdentityByAccessToken($token, $type = null) { return null; } public function getId() { return null; } public function getAuthKey() { return null; } public function validateAuthKey($authKey) { return false; } }
